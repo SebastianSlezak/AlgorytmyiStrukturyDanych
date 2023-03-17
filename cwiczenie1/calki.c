41b42a4f23915d665aa380e7474fa3fc44253835
SebastianSlezak <szalassje@gmail.com>
2023-03-17 18:33:27 +0100

    Projekt całkowanie 0.2 - została dodana implemantacja metody trapezy

--- cwiczenie1/calki.c
+++ cwiczenie1/calki.c
@@ -3,36 +3,44 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+
 #define lp 1000
 
-double c_od,c_do;
+double c_od, c_do;
 
-double f3(double x)
-{
- return -1;
+double f3(double x) {
+    return -1;
 }
+
 double f4(double x) {
-
- return 1;
+    return 1;
 }
 
 
-double prostokaty(double( *f)(double))
-{
-       double krok, p, suma=0;
-       krok=(c_do-c_od)/lp;
-       for(p=c_od; p<c_do; p+=krok)
-               suma+=(*f)(p)*krok;
-       return suma;
+double prostokaty(double( *f)(double)) {
+    double krok, p, suma = 0;
+    krok = (c_do - c_od) / lp;
+    for (p = c_od; p < c_do; p += krok)
+        suma += (*f)(p) * krok;
+    return suma;
 }
 
-double trapezy(double (*f)(double))
-{
-return 1;
+double trapezy(double (*f)(double)) {
+    //liczba trapezów podziałowych
+    const int N = 1000;
+    double s = 0;
+    double dx = (c_do - c_od) / N;
+
+    for (int i = 1; i < N; i++){
+        s += f(c_od + i * dx);
+    }
+
+    s = (s + (*f)(c_od) + (*f)(c_do) / 2) * dx;
+
+    return s;
 }
 
-double mc(double(*f)(double))
-{
- return 1;
+double mc(double(*f)(double)) {
+    return 1;
 }
 
