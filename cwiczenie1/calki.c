5e9a305c2240ef5bb22820f8400d946b48d7358c
SebastianSlezak <szalassje@gmail.com>
2023-03-19 21:31:31 +0100

    Projekt całkowanie 0.4 - została poprawiona implemantacja metody prostokaty oraz zostały dodane do metod f3 i f4 całki nieoznaczone

--- cwiczenie1/calki.c
+++ cwiczenie1/calki.c
@@ -1,5 +1,3 @@
-//ten plik trzeba edytować
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
@@ -9,24 +7,29 @@
 double c_od, c_do;
 
 double f3(double x) {
-    return -1;
+    return exp(-x * x);
 }
 
 double f4(double x) {
-    return 1;
+    return sin(x) / x;
 }
 
 
 double prostokaty(double( *f)(double)) {
-    double krok, p, suma = 0;
-    krok = (c_do - c_od) / lp;
-    for (p = c_od; p < c_do; p += krok)
-        suma += (*f)(p) * krok;
-    return suma;
+    const int N = 1000;
+    double s = 0;
+    double dx = (c_do - c_od) / N;
+
+    for (int i = 1; i <= N; i++) {
+        s += (*f)(c_od + i * dx);
+    }
+
+    s *= dx;
+
+    return s;
 }
 
 double trapezy(double (*f)(double)) {
-    //liczba trapezów podziałowych
     const int N = 1000;
     double s = 0;
     double dx = (c_do - c_od) / N;
